1a4fd7eba78caa631bf08e98907f56112bb87d7a
Adrián E. Córdoba <software.asia@gmail.com>
2025-10-30 15:56:19 -0300

    Config file access.

--- System/Python_Projects/Processor/main.py
+++ System/Python_Projects/Processor/main.py
@@ -21,14 +21,10 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+import yaml
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with open('config/config.yaml', 'r') as config_file:
+        config_data = yaml.safe_load(config_file)
+    print(config_data['environment'])
